7c1e652ae310fadcc02c2e4b40db15265e5c5cc4
JohanComparat <johan.comparat@gmail.com>
2018-01-31 16:19:12 +0100

    v3 initial

---
+++ bin/run_archetypes_XMMSL2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+python3.4 construct_archetypes.py -0.1 0.001 XMMSL 2.
+python3.4 construct_archetypes.py 0.001 0.1  XMMSL 2.
+python3.4 construct_archetypes.py 0.1 0.2    XMMSL 2.
+python3.4 construct_archetypes.py 0.2 0.3    XMMSL 2.
+python3.4 construct_archetypes.py 0.3 0.4    XMMSL 2.
+python3.4 construct_archetypes.py 0.4 0.5    XMMSL 2.
+python3.4 construct_archetypes.py 0.5 0.6    XMMSL 2.
+python3.4 construct_archetypes.py 0.6 0.7    XMMSL 2.
+python3.4 construct_archetypes.py 0.7 0.8    XMMSL 2.
+python3.4 construct_archetypes.py 0.8 0.9    XMMSL 2.
+python3.4 construct_archetypes.py 0.9 1.0    XMMSL 2.
+python3.4 construct_archetypes.py 1.0 1.2    XMMSL 2.
+python3.4 construct_archetypes.py 1.2 1.4    XMMSL 2.
+python3.4 construct_archetypes.py 1.4 1.6    XMMSL 2.
+python3.4 construct_archetypes.py 1.6 1.8    XMMSL 2.
+python3.4 construct_archetypes.py 1.8 2.0    XMMSL 2.
+python3.4 construct_archetypes.py 2.0 2.3    XMMSL 2.
+python3.4 construct_archetypes.py 2.3 2.6    XMMSL 2.
+python3.4 construct_archetypes.py 2.6 3.0    XMMSL 2.
+python3.4 construct_archetypes.py 3.0 4.0    XMMSL 2.
+python3.4 construct_archetypes.py 4.0 5.0    XMMSL 2.
+python3.4 construct_archetypes.py 5.0 6.0    XMMSL 2.
