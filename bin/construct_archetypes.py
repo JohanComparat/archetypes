882d062ec8e35c6d14dfeaffe90222f4200fb809
JohanComparat <johan.comparat@gmail.com>
2019-06-07 10:50:51 +0200

    archetype script

--- bin/construct_archetypes.py
+++ bin/construct_archetypes.py
@@ -4,6 +4,7 @@ t0 = time.time()
 import numpy as n
 import matplotlib
 matplotlib.use('Agg')
+matplotlib.rcParams.update({'font.size': 14})
 import matplotlib.pyplot as p
 from scipy.interpolate import interp1d
 from SetCoverPy import setcover, mathutils
@@ -11,26 +12,22 @@ import sys
 from scipy.stats import scoreatpercentile
 import os
 import pickle
+import SpectraStackingEBOSS as sse
 
 version = 'v1'
 sdss_dir = os.path.join(os.environ['HOME'], "SDSS")
-stack_dir = os.path.join(sdss_dir, "stack", "X_AGN")
+stack_dir = os.path.join(sdss_dir, "stacks", "X_AGN")
 archetype_dir = os.path.join(sdss_dir, "archetypes", version)
 
-logwlmin = 2.9. 
-logwlmax = 4.0. 
-d_lambda = 0.0001 
-masterwave = 10**n.arange(logwlmin, logwlmax, d_lambda) 
-
 def stack_it(specList ):
- outfile = join(spec_dir, os.path.basename(specList)[:-4]+".stack")
+ outfile = os.path.join(stack_dir, os.path.basename(specList)[:-4]+".stack")
  print(outfile)
  test_D = n.loadtxt(specList, unpack=True)
  print(len(test_D[0]))
  if len(test_D[0])>10:
   stack=sse.SpectraStackingEBOSS(specList, outfile )
-  stack.createStackMatrix()
-  stack.stackSpectra()
+  #stack.createStackMatrix()
+  #stack.stackSpectra()
   return stack
 
 file_list = n.array([
@@ -43,132 +40,114 @@ file_list = n.array([
 	os.path.join(stack_dir, "full_QSO_zmin_00_zmax_50.asc"   ) ,
 	os.path.join(stack_dir, "full_STAR_zmin_00_zmax_50.asc"  ) ])
 
-stack = {}
-for file_input in file_list:
- stack[file_input] = stack_it(file_input)
 
-sn_min = 2.
-
-out_dir = os.path.join( os.environ['OBS_REPO'], 'archetypes', version, gal_type )
-
-
-allflux = n.loadtxt(os.path.join(out_dir, "allflux_"+name+".txt") )
-allivar = n.loadtxt(os.path.join(out_dir, "allivar_"+name+".txt") )
-masterwave = n.loadtxt(os.path.join(out_dir, "masterwave_"+name+".txt"))
-
-print("interpolates on the common grid", time.time()-t0, 's', allflux.shape)
-index_wave_all = n.searchsorted(masterwave, [masterwave[0]+100,masterwave[-1]-100])
-tmpflux = allflux.T[index_wave_all[0]:index_wave_all[1],:]
-tmpivar = allivar.T[index_wave_all[0]:index_wave_all[1],:]
-tmpwave = masterwave[index_wave_all[0]:index_wave_all[1]]
-tmploglam = n.log10(tmpwave)
-
-#median_sn = n.zeros(nGal)
-#for i in n.arange(nGal):
-    #iuse = (n.where(tmpivar[:,i]>0))[0]
-    #if iuse.size>0:
-        #median_sn[i] = n.median(tmpflux[iuse,i]*n.sqrt(tmpivar[iuse,i]))
-
-median_sn = n.median( tmpflux*tmpivar, axis=0)
- 
-iuse = n.where( median_sn>sn_min)[0]
-
-# smooth the data
-#n_pixels = len(tmpwave)
-#newwave = n.median(tmpwave.reshape(n_pixels//4, 4), axis=1)
-#newflux = n.sum(tmpflux.reshape(n_pixels//4, 4, tmpflux.shape[1]), axis=1)
-#newivar = 1./n.sum(1./tmpivar.reshape(n_pixels//4, 4, tmpflux.shape[1]), axis=1)
-
-newwave = tmpwave
-newflux = tmpflux
-newivar = tmpivar
-tmpchi2 = n.zeros((iuse.size, iuse.size))
-A = n.zeros((iuse.size, iuse.size))
-
-print("creates the matrix", time.time()-t0, 's', newflux.shape)
-tmp_yerr = 1./n.sqrt(newivar[:, iuse].T.reshape(iuse.size, newwave.size))
-tmp_y = newflux[:,iuse].T
-for i in n.arange(iuse.size):
-    #print(i)
-    tmp_x = newflux[:, iuse[i]].T.reshape(1,newwave.size)
-    tmp_xerr = 1./n.sqrt(newivar[:, iuse[i]].T.reshape(1,newwave.size))
-    #print(tmp_x, tmp_y, tmp_xerr, tmp_yerr)
-    A_tmp, chi2_tmp = mathutils.quick_amplitude(tmp_x, tmp_y, tmp_xerr, tmp_yerr)
-    A[i,:] = A_tmp
-    tmpchi2[i,:] = chi2_tmp
-    
-chi2 = tmpchi2/(iuse.size-1) # reduced chi2
-
-#pcs = n.array([1,10,25,50,75,90,99])
-#pcs = n.array([16,17,18,19,20,21,22, 23, 24])
-#scrs = scoreatpercentile(n.ravel(chi2), [16,17,18,19,20,21,22, 23, 24])
-
-scr = scoreatpercentile(n.ravel(chi2), 20 )
-
-chi2_min = scr # 0.05 # the minimum distance, the only free paramter
-a_matrix = chi2<chi2_min # relationship matrix
-cost = n.ones(iuse.size)
-g = setcover.SetCover(a_matrix, cost)
-# I'm using greedy just for demonstration
-# g.greedy()
-# SolveSCP() should be used to generate near-optimal solution
-g.SolveSCP()
-# These are the archetypes
-iarchetype = n.nonzero(g.s)[0]
-print( scr, len(iarchetype), time.time()-t0, 's')
-
-n_rep = n.sum(a_matrix[:, iarchetype], axis=0) # how many covered by the archetype?
-isort = n.argsort(n_rep)[::-1]
-
-tmpmedian = n.zeros((iarchetype.size, masterwave.size))
-
-# These are the archetypal composites we want to use as the initial guess
-for i in n.arange(iarchetype.size):
-    itmp = a_matrix[:, iarchetype[i]] # These are the instances represented by the archetype
-    for j in n.arange(masterwave.size):
-        thisflux = allflux.T[j,iuse[itmp]]
-        tmpmedian[i, j] = n.median(thisflux[thisflux!=0]) # Only use the objects that have this wavelength covered
-
-#from _pickle import cPickle
-#cPickle.dum
-
-imax = n.max(isort)
-print('imax', imax)
-p.clf()
-fig = p.figure(figsize=(10,imax*5))
-fig.subplots_adjust(hspace=0)
-for i in n.arange(0,imax,1):
-	ax = fig.add_subplot(imax+1,1,i+1)
-	ax.plot(masterwave[::3], tmpmedian[isort[i],:][::3] , label = 'nSpec=' + str(n.count_nonzero(a_matrix[:,iarchetype[isort[i]]])) )
-	ax.set_xlim(masterwave[10], masterwave[-10])
-	#ax.set_ylim(-0.1, 3)
-	ax.set_xticks([])
-	ax.axvline(1215, color='b', ls='dashed', label='1215 Lya')
-	ax.axvline(1546, color='c', ls='dashed', label='1546 CIV')
-	ax.axvline(2800, color='m', ls='dashed', label='2800 MgII')
-	ax.axvline(3727, color='g', ls='dashed', label='3727 [OII]')
-	ax.axvline(5007, color='r', ls='dashed', label='5007 [OIII]')
-	ax.axvline(6565, color='k', ls='dashed', label='6565 Ha')
-	print(i, n.count_nonzero(a_matrix[:,iarchetype[isort[i]]]))
-	ax.grid()
-	ax.legend(frameon=False, loc=0)
-
-#fig.set_xticks([2000, 3000, 4000, 5000])
-fig.savefig(os.path.join(out_dir, "figureArchetypes_"+name+"_snMin"+str(sn_min)+".png"))
-#fig.savefig(os.path.join(os.environ['HOME'], 'wwwDir', 'sdss', 'elg', 'test.png'))
-p.clf()
-
-n.savetxt(os.path.join(out_dir, "archetypes_"+name+"_snMin"+str(sn_min)+".txt")   , n.vstack((masterwave, tmpmedian)))
-
+###############################################
+###############################################
+# NOW ARCHETYPE
+###############################################
+###############################################
 class ObjIds:
-    def __init__(self, imax, iuse):
-        self.imax = imax
-        self.iuse = iuse
-        
-f = open(os.path.join(out_dir, "indexArchetypes_"+name+"_snMin"+str(sn_min)+".pkl"), 'wb')
-obj = ObjIds(imax, iuse)
-pickle.dump(obj, f)
-f.close()
+	def __init__(self, imax, iuse):
+		self.imax = imax
+		self.iuse = iuse
+		self.n_rep = n_rep
 
-#n.savetxt(os.path.join('..', 'archetypes_v0', "archetypes.txt")   , n.vstack((masterwave, tmpmedian)))
+def make_archetype(stack, file_input, sn_min = 2.):
+	print('starts archetype for',file_input, time.time()-t0)
+	out_name = 'archetype_'+os.path.basename(stack[file_input].out_file)+'_snMin_'+str(sn_min)
+	allflux    = n.loadtxt(stack[file_input].out_file+'.specMatrix.dat'      , unpack=True )      #, specMatrix)
+	allivar    = n.loadtxt(stack[file_input].out_file+'.specMatrixErr.dat'   , unpack=True )**(-1)#, specMatrixErr)
+	masterwave = stack[file_input].wave 
+	tmploglam = n.log10(masterwave)
+	# filter data
+	median_sn = n.median( allflux*allivar, axis=0)
+	iuse = n.where( median_sn>sn_min)[0]
+	#
+	tmpchi2 = n.zeros((iuse.size, iuse.size))
+	A = n.zeros((iuse.size, iuse.size))
+	print("creates the matrix", time.time()-t0, 's', allflux.shape)
+	tmp_yerr = 1./n.sqrt(allivar[:, iuse].T.reshape(iuse.size, masterwave.size))
+	tmp_y = allflux[:,iuse].T
+	for i in n.arange(iuse.size):
+		#print(i)
+		tmp_x = allflux[:, iuse[i]].T.reshape(1,masterwave.size)
+		tmp_xerr = 1./n.sqrt(allivar[:, iuse[i]].T.reshape(1,masterwave.size))
+		#print(tmp_x, tmp_y, tmp_xerr, tmp_yerr)
+		A_tmp, chi2_tmp = mathutils.quick_amplitude(tmp_x, tmp_y, tmp_xerr, tmp_yerr)
+		A[i,:] = A_tmp
+		tmpchi2[i,:] = chi2_tmp
+		
+	chi2 = tmpchi2/(iuse.size-1) # reduced chi2
+	#pcs = n.array([1,10,25,50,75,90,99])
+	#pcs = n.array([16,17,18,19,20,21,22, 23, 24])
+	#scrs = scoreatpercentile(n.ravel(chi2), [16,17,18,19,20,21,22, 23, 24])
+	scr = scoreatpercentile(n.ravel(chi2), 20 )
+
+	chi2_min = scr # 0.05 # the minimum distance, the only free paramter
+	a_matrix = chi2<chi2_min # relationship matrix
+	cost = n.ones(iuse.size)
+	g = setcover.SetCover(a_matrix, cost)
+	# I'm using greedy just for demonstration
+	# g.greedy()
+	# SolveSCP() should be used to generate near-optimal solution
+	g.SolveSCP()
+	# These are the archetypes
+	iarchetype = n.nonzero(g.s)[0]
+	print( scr, len(iarchetype), time.time()-t0, 's')
+
+	# sort archeypes against the number of spectra they represent
+	n_rep = n.sum(a_matrix[:, iarchetype], axis=0) # how many covered by the archetype?
+	isort = n.argsort(n_rep)[::-1]
+	print(n_rep)
+	archetype_median = n.zeros((iarchetype.size, masterwave.size))
+
+	# These are the archetypal composites we want to use as the initial guess
+	for i in n.arange(iarchetype.size):
+		itmp = a_matrix[:, iarchetype[i]] # These are the instances represented by the archetype
+		for j in n.arange(masterwave.size):
+			thisflux = allflux[j,iuse[itmp]]
+			archetype_median[i, j] = n.median(thisflux[thisflux!=0]) # Only use the objects that have this wavelength covered
+
+	#from _pickle import cPickle
+	#cPickle.dum
+
+	imax = n.max(isort)
+	print('imax', imax)
+	p.clf()
+	fig = p.figure(figsize=(10,imax*5))
+	fig.subplots_adjust(hspace=0)
+	for i in n.arange(0,imax,1):
+		ax = fig.add_subplot(imax+1,1,i+1)
+		ax.plot(masterwave[::3], archetype_median[isort[i],:][::3] , label = 'nRep=' + str(n_rep[isort[i]) )
+		ax.set_xlim(masterwave[10], masterwave[-10])
+		#ax.set_ylim(-0.1, 3)
+		ax.set_xticks([])
+		ax.axvline(1215, color='b', ls='dashed', label='1215 Lya')
+		ax.axvline(1546, color='c', ls='dashed', label='1546 CIV')
+		ax.axvline(2800, color='m', ls='dashed', label='2800 MgII')
+		ax.axvline(3727, color='g', ls='dashed', label='3727 [OII]')
+		ax.axvline(5007, color='r', ls='dashed', label='5007 [OIII]')
+		ax.axvline(6565, color='k', ls='dashed', label='6565 Ha')
+		print(i, n.count_nonzero(a_matrix[:,iarchetype[isort[i]]]))
+		ax.grid()
+		ax.legend(frameon=False, loc=0)
+
+
+	fig.savefig( os.path.join(archetype_dir, "figure_archetypes_"+out_name +".png") )
+	p.clf()
+
+	n.savetxt(os.path.join(archetype_dir, "archetypes_"+out_name+".txt")   , n.vstack((masterwave, archetype_median)))
+	
+	f = open(os.path.join(archetype_dir, "index_archetypes_"+out_name+".pkl"), 'wb')
+	obj = ObjIds(imax, iuse, n_rep)
+	pickle.dump(obj, f)
+	f.close()
+
+
+stack = {}
+#for file_input in file_list:
+file_input = file_list[2]
+stack[file_input] = stack_it(file_input)
+make_archetype(stack, file_input, sn_min = 2.)
+#n.savetxt(os.path.join('..', 'archetypes_v0', "archetypes.txt")   , n.vstack((masterwave, archetype_median)))
 # DATA = n.loadtxt(os.path.join('..', 'archetypes_v0', "archetypes.txt") )
--- bin/construct_matrices_X_AGN.py
+++ bin/construct_matrices_X_AGN.py
@@ -7,81 +7,133 @@ from scipy.interpolate import interp1d
 import os
 import astropy.io.fits as fits
 import pickle
+import spectres
+import SpectraStackingEBOSS as sse
 
-version = 'v3'
+version = 'v1'
+sdss_dir = = os.path.join(os.environ['HOME'], "SDSS")
+archetype_dir = os.path.join(sdss_dir, "archetypes", version)
 
-# parameters of the run
-zmins = n.arange(0,3.1,0.5)[:-1]
-zmaxs = n.arange(0,3.1,0.5)[1:]
-#gal_type = sys.argv[3]
-# ELG, LRG, X_AGN, QSO
-#print('sys.argv', sys.argv)
-
-
-SN_threshold = 0.5
-wlmin = 3700.
-wlmax = 9000.
-wlmin_rf = int(wlmin/(1+zmax))
-wlmax_rf = int(wlmax/(1+zmin))
-d_lambda = 1. 
-masterwave = n.arange(wlmin_rf, wlmax_rf, d_lambda) #0.5)
-
-path_2_cats = os.path.join(os.environ['HOME'], 'data', 'spiders', 'agn')
-path_2_stack_lists = os.path.join(os.environ['HOME'], 'SDSS/stacks/X_AGN')
+logwlmin = 2.9. #1000.
+logwlmax = 4.0. #10000.
+d_lambda = 0.0001 
+masterwave = 10**n.arange(logwlmin, logwlmax, d_lambda) 
 
 
-path_2_XMMSL2 = os.path.join(path_2_cats, 'SPIDERS_XMMSL2_Xray_NWAY_ALLWISE_SDSSv5b_SpecDR14_SpecDR16_with_VI_1rowperXray_inDR16wSEQUELS_COMPLETE.fits')
-path_2_2RXS   = os.path.join(path_2_cats, 'SPIDERS_2RXS_Xray_NWAY_ALLWISE_SDSSv5b_SpecDR14_SpecDR16_with_VI_1rowperXray_inDR16wSEQUELS_COMPLETE.fits')
-path_2_XXL    = os.path.join(path_2_cats, 'Menzel16_spAll_XXL_plate.fits')
-path_2_S82X   = os.path.join(path_2_cats, 'LaMassa19_spAll_S82X_plate.fits')
-
-cat_XMMSL2 = fits.open(path_2_XMMSL2)[1].data
-cat_2RXS   = fits.open(path_2_2RXS  )[1].data
-cat_XXL    = fits.open(path_2_XXL   )[1].data
-cat_S82X   = fits.open(path_2_S82X  )[1].data
-
-# join the 4 catalogs into a single list for each category
-
-# XMMSL2 lists
-survey = 'XMMSL2'
-cat = cat_XMMSL2
-z_best = cat['Z_BEST']
-z_conf = cat['CONF_BEST']
-print(survey)
-for zmin, zmax in zip(zmins, zmaxs):
-	print('=================================================')
-	print(zmin, '<z<', zmax)
-	selection = (z_best > zmin) & (z_best < zmax) & (z_conf>=3)
-	all_categories = n.unique(cat['CLASS_BEST'][selection])              
-	for category in all_categories:
-		ok_i = (cat['CLASS_BEST']==category) & (selection)
-		N_obj = len(ok_i.nonzero()[0])
-		print(category, N_obj)
-		if N_obj>0:
-			name = survey+"_"+category+"_zmin_"+str(int(10*zmin)).zfill(2)+"_zmax_"+str(int(10*zmax)).zfill(2)+"_Nlt_"+str(int(Nspec_max))
-			print(name)
-			DATA = n.transpose([ cat['PLATE_BEST'][ok_i], cat['MJD_BEST'][ok_i], cat['FIBERID_BEST'][ok_i], z_best[ok_i] ])
-			n.savetxt(os.path.join(path_2_stack_lists, name), DATA)
-
-sys.exit()
-
-n.unique(cat_2RXS['CLASS_BEST'])                                                                                                                                                          
+file_list = n.array([
+	"/home/comparat/SDSS/stacks/X_AGN/full_BLAGN_zmin_00_zmax_50.asc"  ,
+	#"/home/comparat/SDSS/stacks/X_AGN/full_BLANK_zmin_00_zmax_50.asc"  ,
+	"/home/comparat/SDSS/stacks/X_AGN/full_BLAZAR_zmin_00_zmax_50.asc" ,
+	"/home/comparat/SDSS/stacks/X_AGN/full_BLLAC_zmin_00_zmax_50.asc"  ,
+	"/home/comparat/SDSS/stacks/X_AGN/full_GALAXY_zmin_00_zmax_50.asc" ,
+	"/home/comparat/SDSS/stacks/X_AGN/full_NLAGN_zmin_00_zmax_50.asc"  ,
+	"/home/comparat/SDSS/stacks/X_AGN/full_NONE_zmin_00_zmax_50.asc"   ,
+	"/home/comparat/SDSS/stacks/X_AGN/full_QSO_zmin_00_zmax_50.asc"    ,
+	"/home/comparat/SDSS/stacks/X_AGN/full_STAR_zmin_00_zmax_50.asc"   ])
 
 
-n.unique(cat_XXL['TDVI_CLASS_PERSON'])                                                                                                                                                          
-# chararray(['', '     QSO', '    NONE', '    STAR', '  BLAZAR', '  GALAXY', ' QSO_BAL'], dtype='<U16')
+def stack_it(specList ):
+	outfile = join(spec_dir, os.path.basename(specList)[:-4]+".stack")
+	print(outfile)
+	test_D = n.loadtxt(specList, unpack=True)
+	print(len(test_D[0]))
+	if len(test_D[0])>10:
+		stack=sse.SpectraStackingEBOSS(specList, outfile )
+		stack.createStackMatrix()
 
-n.unique(cat_S82X['TDVI_CLASS_PERSON'])                                                                                                                                                          
-# chararray(['', '     QSO', '    NONE', '  GALAXY'], dtype='<U16')
+		#stack.stackSpectra()
 
-cat = cat_XMMSL2
-print(cat['PLATE_BEST'][:10], cat['MJD_BEST'][:10], cat['FIBERID_BEST'][:10])
+for file_input in file_list[4:]:
+	stack_it(file_input)
 
-cat = cat_2RXS
-print(cat['PLATE_BEST'][:10], cat['MJD_BEST'][:10], cat['FIBERID_BEST'][:10])
 
-cat = cat_XXL
-print(cat['DR16_PLATE'][:10], cat['DR16_MJD'][:10], cat['DR16_FIBERID'][:10])
+def create_matrix(path_2_list):
+path_2_list = all_lists[0]
 
-cat = cat_S82X
-print(cat['DR16_PLATE'][:10], cat['DR16_MJD'][:10], cat['DR16_FIBERID'][:10])
+
+nGal = len(ok.nonzero()[0])
+# 1. for each element of the list get the spectrum
+# 2. in the rest frame force it to the masterwave
+
+print("run name:",name)
+print("considering", nGal)
+SSv5b_SpecDR14_SpecDR16_with_VI_1rowperXray_inDR16wSEQUELS_COMPLETE.fits')
+allflux = n.zeros( ( nGal, len(masterwave)) )v5b_SpecDR14_SpecDR16_with_VI_1rowperXray_inDR16wSEQUELS_COMPLETE.fits')
+allivar = n.zeros( ( nGal, len(masterwave)) )
+
+print("gets the spectra to construct the matrix")
+for index in range(nGal) :# = 0
+	path_1 = get_path_to_spectrum_v5_10_10(cat['PLATE'][ok][index], cat['MJD'][ok][index], cat['FIBERID'][ok][index])
+	path_2 = get_path_to_spectrum_26(cat['PLATE'][ok][index], cat['MJD'][ok][index], cat['FIBERID'][ok][index])
+	path_3 = get_path_to_spectrum_v5_10_0(cat['PLATE'][ok][index], cat['MJD'][ok][index], cat['FIBERID'][ok][index])
+	choice = n.array([os.path.isfile(path_1), os.path.isfile(path_2), os.path.isfile(path_3)])
+	pathes = n.array([path_1, path_2, path_3])
+	print(pathes, choice)
+	if choice.any():
+		path = pathes[choice][0]
+		print( "exists" )
+		redshift = cat['Z'][ok][index]
+		wl, fl, iv = get_spec(path)
+		#wl_rf = wl / (1+redshift)
+		w_new = masterwave*(1+redshift)
+		itp_fl = interp1d( n.hstack((100.,wl[0]-1.,wl,wl[-1]+1,20000.)), n.hstack((0.,0.,wl*fl,0.,0.)) )
+		itp_iv = interp1d( n.hstack((100.,wl[0]-1.,wl,wl[-1]+1,20000.)), n.hstack((0.,0.,iv,0.,0.)) )
+		try :
+			f_new = itp_fl(w_new)/w_new
+			iv_new = itp_iv(w_new)
+			allflux[index] = f_new
+			allivar[index] = iv_new
+		except(ValueError):	
+			pass
+		max_"+str(int(10*zmax)).zfill(2)+"_Nlt_"+str(int(Nspec_max))
+out_dir = os.path.join( os.environ['OBS_REPO'], 'archetypes', version, gal_type )
+if os.path.isdir(out_dir)==False:], cat['FIBERID_BEST'][ok_i], z_best[ok_i] ])
+  os.system('mkdir -p '+out_dir)
+  
+n.savetxt(os.path.join(out_dir, "allflux_"+name+".txt")   , allflux)
+n.savetxt(os.path.join(out_dir, "allivar_"+name+".txt")   , allivar)
+n.savetxt(os.path.join(out_dir, "masterwave_"+name+".txt"), masterwave)                                                                                                            
+
+
+class ObjIds:                                                                                                                  
+    def __init__(self, plate, mjd, fiberid): QSO_BAL'], dtype='<U16')
+        self.plate = plate
+        self.mjd = mjd                                                                                                                   
+        self.fiberid = fiberid
+        
+f = open(os.path.join(out_dir, "specObj_"+name+".pkl"), 'wb')
+obj = ObjIds(cat['PLATE'][ok], cat['MJD'][ok], cat['FIBERID'][ok])
+pickle.dump(obj, f)
+f.close()
+
+
+	wavelength_spectrum = np.hstack(( 
+		wave[0]-10, 
+		wave[0]-5,
+		np.min(inter_lambda)-10,
+		np.min(inter_lambda)-5,
+		inter_lambda,
+		np.max(inter_lambda)+5,
+		np.max(inter_lambda)+10,
+		wave[-1]+5, 
+		wave[-1]+10
+		))
+	#
+	flux_spectrum = np.hstack(( 
+		inter_flux[0],inter_flux[0],inter_flux[0],inter_flux[0],
+		inter_flux,
+		inter_flux[-1],inter_flux[-1],inter_flux[-1],inter_flux[-1]
+		))
+	#
+	print(flux_spectrum.shape)
+	flux_error_spectrum = flux_spectrum/100.
+	#
+	final_spectrum, final_spectrum_err = sp.spectres(
+		wave, 
+		wavelength_spectrum, 
+		flux_spectrum, 
+		flux_error_spectrum )
+
+
+
+
